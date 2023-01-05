581695d8881775cbddb87e2f849f7a372265fdcf
ailopez- <aitorlpzaudikana@gmail.com>
2023-01-05 18:48:33 +0100

    CPP_00 ex00

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+std::string to_upper;
+
+int		main(int argc, char **argv)
+{
+	if (argc != 2)
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+	else
+	{
+		std::string str = argv[1];
+		
+		std::transform(str.begin(), str.end(), str.begin(),::toupper);
+		std::cout<<str<<std::endl;
+	}
+}
