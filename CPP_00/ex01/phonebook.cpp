18328e40ce41caabe2902bb02d84efc9f2d429d2
ailopez- <aitorlpzaudikana@gmail.com>
2023-01-08 13:08:22 +0100

    go

--- CPP_00/ex01/phonebook.cpp
+++ CPP_00/ex01/phonebook.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include <string>
+#include "phonebook.hpp"
 
-int		main(int argc, char **argv)
+// ------// Class Phonebook
+
+void Phonebook::phonebook(std::string name)
 {
+	this->name = name;
+}
 
-	
+// ------// Class Contact
+
+void Contact::contact(int id)
+{
+	this->id = id;
+}
+
+void	Contact::set_first_name(std::string name)
+{
+	this->first_name = name;
+}
+
+std::string	Contact::get_first_name()
+{
+	return (first_name);
 }
